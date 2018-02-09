853fbf3171e8c888e706acf298b62b71b3c6009f
Fernando Junio Cunha e Sousa <fernando.sousa@lggo.com.br>
2018-02-09 13:45:37 -0200

    Adicionando as funções para captura de eventos

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,15 +1,32 @@
 import React, {Component} from 'react'
 import PageHeader from '../template/pageHeader'
-import Form from './todoForms'
-import List from './todoList'
+import TodoForm from './todoForms'
+import TodoList from './todoList'
 
 export default class Todo extends Component {
+    constructor(props){
+        super(props)
+        this.state = {description: '' , list: [] }
+        this.handleChange = this.handleChange.bind(this)
+        this.handleAdd = this.handleAdd.bind(this)
+    }
+
+    handleChange(e) {
+        this.setState({...this.state, description: e.target.value})
+    }
+    
+    handleAdd(){
+        console.log(this.state.description)
+    }
+
     render(){
         return (
             <div>
                 <PageHeader name='Tarefas' small='Cadastro' />
-                <Form />
-                <List />
+                <TodoForm handleAdd={this.handleAdd} 
+                          description={this.state.description}
+                          handleChange={this.handleChange} />
+                <TodoList />
             </div>
         )
     }
