5a1065b5a0b26e0606237c97c07a224c4f775b91
Fernando Junio <fjcs7@hotmail.com>
2018-02-08 22:07:19 -0200

    Criando os components form e list

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,11 +1,15 @@
 import React, {Component} from 'react'
 import PageHeader from '../template/pageHeader'
+import Form from './todoForms'
+import List from './todoList'
 
 export default class Todo extends Component {
     render(){
         return (
             <div>
                 <PageHeader name='Tarefas' small='Cadastro' />
+                <Form />
+                <List />
             </div>
         )
     }
